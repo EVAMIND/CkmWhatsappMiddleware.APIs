701a5cdde4001624dcf5147d8c0f6eb362b067de
Matheus-Evamind <107504356+Matheus-Evamind@users.noreply.github.com>
2024-05-03 15:38:11 -0300

    feat: endpoints

--- ApiApplications/Interfaces/IGupShupApplication.cs
+++ ApiApplications/Interfaces/IGupShupApplication.cs
@@ -1,6 +1,19 @@
-﻿namespace CkmWhatsAppMiddleware.APIs.ApiApplications.Interfaces;
+﻿using CkmICDServices.DTOs.TransferObjects.Message.Request.Base;
+using CkmICDServices.DTOs.TransferObjects.Message.Request;
+using CkmICDServices.DTOs.TransferObjects.Message;
+using CkmICDServices.DTOs.TransferObjects.Template;
+
+namespace CkmWhatsAppMiddleware.APIs.ApiApplications.Interfaces;
 
 public interface IGupShupApplication
 {
     Task<bool> GetWalletBalanceAsync(string apiKey, string token);
+    Task<MessageTemplateResultView> GetAllTemplatesByApiKey(string apiKey, string sourceName, string token);
+    Task<bool> SendTemplateToCustomers(string apiKey, List<string> phones, Guid templateId, string token);
+    Task<MessageView> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
+    Task<MessageView> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
+    Task<MessageView> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
+    Task<MessageView> SendFileMessage(string apiKey, BaseMessageRequestDTO<FileMessageRequestDTO> messageRequest, string token);
+    Task<MessageView> SendVideoMessage(string apiKey, BaseMessageRequestDTO<VideoMessageRequestDTO> messageRequest, string token);
+    Task<MessageView> SendStickerMessage(string apiKey, BaseMessageRequestDTO<StickerMessageRequestDTO> messageRequest, string token);
 }
--- ApiRepositories/Interfaces/IGupShupRepository.cs
+++ ApiRepositories/Interfaces/IGupShupRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace CkmWhatsAppMiddleware.APIs.ApiRepositories.Interfaces;
+﻿
+using CkmICDServices.DTOs.TransferObjects.Message.Request;
+using CkmICDServices.DTOs.TransferObjects.Message.Request.Base;
+
+namespace CkmWhatsAppMiddleware.APIs.ApiRepositories.Interfaces;
 
 public interface IGupShupRepository
 {
+    Task<HttpResponseMessage> GetAllTemplatesByApiKey(string apiKey, string sourceName, string token);
     Task<HttpResponseMessage> GetWalletBalanceAsync(string apiKey, string token);
+    Task<bool> SendTemplateToCustomers(List<string> phones, Guid templateId, string apiKey, string token);
+    Task<HttpResponseMessage> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
+    Task<HttpResponseMessage> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
+    Task<HttpResponseMessage> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
+    Task<HttpResponseMessage> SendFileMessage(string apiKey, BaseMessageRequestDTO<FileMessageRequestDTO> messageRequest, string token);
+    Task<HttpResponseMessage> SendVideoMessage(string apiKey, BaseMessageRequestDTO<VideoMessageRequestDTO> messageRequest, string token);
+    Task<HttpResponseMessage> SendStickerMessage(string apiKey, BaseMessageRequestDTO<StickerMessageRequestDTO> messageRequest, string token);
 }
