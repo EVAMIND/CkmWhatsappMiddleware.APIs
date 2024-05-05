d73e8d2d2d2686daa2c984b37098ceea78404d81
Matheus-Evamind <107504356+Matheus-Evamind@users.noreply.github.com>
2024-05-05 16:13:16 -0300

    fix: ajuste de parâmetro - Template Request

--- ApiApplications/Interfaces/IGupShupApplication.cs
+++ ApiApplications/Interfaces/IGupShupApplication.cs
@@ -2,6 +2,7 @@
 using CkmICDServices.DTOs.TransferObjects.Message.Request;
 using CkmICDServices.DTOs.TransferObjects.Message;
 using CkmICDServices.DTOs.TransferObjects.Template;
+using CkmICDServices.DTOs.TransferObjects.Template.Request;
 
 namespace CkmWhatsAppMiddleware.APIs.ApiApplications.Interfaces;
 
@@ -9,7 +10,7 @@ public interface IGupShupApplication
 {
     Task<bool> GetWalletBalanceAsync(string apiKey, string token);
     Task<MessageTemplateResultView> GetAllTemplatesByApiKey(string apiKey, string sourceName, string token);
-    Task<bool> SendTemplateToCustomers(string apiKey, List<string> phones, Guid templateId, string token);
+    Task<bool> SendTemplateToCustomers(MessageTemplateRequestView messageTemplate, string apiKey, Guid templateId, string token);
     Task<MessageView> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
     Task<MessageView> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
     Task<MessageView> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
--- ApiRepositories/Interfaces/IGupShupRepository.cs
+++ ApiRepositories/Interfaces/IGupShupRepository.cs
@@ -1,6 +1,7 @@
 ﻿
 using CkmICDServices.DTOs.TransferObjects.Message.Request;
 using CkmICDServices.DTOs.TransferObjects.Message.Request.Base;
+using CkmICDServices.DTOs.TransferObjects.Template.Request;
 
 namespace CkmWhatsAppMiddleware.APIs.ApiRepositories.Interfaces;
 
@@ -8,7 +9,7 @@ public interface IGupShupRepository
 {
     Task<HttpResponseMessage> GetAllTemplatesByApiKey(string apiKey, string sourceName, string token);
     Task<HttpResponseMessage> GetWalletBalanceAsync(string apiKey, string token);
-    Task<bool> SendTemplateToCustomers(List<string> phones, Guid templateId, string apiKey, string token);
+    Task<bool> SendTemplateToCustomers(MessageTemplateRequestView messageTemplate, Guid templateId, string apiKey, string token);
     Task<HttpResponseMessage> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
     Task<HttpResponseMessage> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
     Task<HttpResponseMessage> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
