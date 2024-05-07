403cb16d51a448a4a43de6d19eca4781d3c2316d
Matheus-Evamind <107504356+Matheus-Evamind@users.noreply.github.com>
2024-05-07 14:08:39 -0300

    feat: ajuste de retorno

--- ApiApplications/Interfaces/IGupShupApplication.cs
+++ ApiApplications/Interfaces/IGupShupApplication.cs
@@ -10,11 +10,11 @@ public interface IGupShupApplication
 {
     Task<bool> GetWalletBalanceAsync(string apiKey, string token);
     Task<MessageTemplateResultView> GetAllTemplatesByApiKey(string apiKey, string sourceName, string token);
-    Task<bool> SendTemplateToCustomers(MessageTemplateRequestView messageTemplate, string apiKey, Guid templateId, string token);
-    Task<MessageView> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
-    Task<MessageView> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
-    Task<MessageView> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
-    Task<MessageView> SendFileMessage(string apiKey, BaseMessageRequestDTO<FileMessageRequestDTO> messageRequest, string token);
-    Task<MessageView> SendVideoMessage(string apiKey, BaseMessageRequestDTO<VideoMessageRequestDTO> messageRequest, string token);
-    Task<MessageView> SendStickerMessage(string apiKey, BaseMessageRequestDTO<StickerMessageRequestDTO> messageRequest, string token);
+    Task<bool> SendTemplateToCustomers(BaseMessageRequestDTO<MessageTemplateRequestView> messageTemplateRequest, string apiKey, string token);
+    Task<MessageInboundView> SendTextMessage(string apiKey, BaseMessageRequestDTO<TextMessageRequestDTO> messageRequest, string token);
+    Task<MessageInboundView> SendAudioMessage(string apiKey, BaseMessageRequestDTO<AudioMessageRequestDTO> messageRequest, string token);
+    Task<MessageInboundView> SendImageMessage(string apiKey, BaseMessageRequestDTO<ImageMessageRequestDTO> messageRequest, string token);
+    Task<MessageInboundView> SendFileMessage(string apiKey, BaseMessageRequestDTO<FileMessageRequestDTO> messageRequest, string token);
+    Task<MessageInboundView> SendVideoMessage(string apiKey, BaseMessageRequestDTO<VideoMessageRequestDTO> messageRequest, string token);
+    Task<MessageInboundView> SendStickerMessage(string apiKey, BaseMessageRequestDTO<StickerMessageRequestDTO> messageRequest, string token);
 }
